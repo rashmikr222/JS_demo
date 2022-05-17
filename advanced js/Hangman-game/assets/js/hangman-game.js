c716d39fc172b91242e1988098ab09f536568690
rashmikr222 <rashmi.kr@dollarbirdinc.com>
2022-05-17 12:04:19 +0530

    hangman-game

--- advanced js/Hangman-game/assets/js/hangman-game.js
+++ advanced js/Hangman-game/assets/js/hangman-game.js
@@ -1,7 +1,7 @@
 //1. create a constructor function for the hangman game
 //2. Setup two attributes.One for the word itself.Another for the number of guesses allowed.
 // 3.Create two instances of it and print both to the console
- 
+
 // function Hangman(word,numberOfGuess){
 // this.word = word;
 // this.numberOfGuess = numberOfGuess
@@ -31,7 +31,7 @@
 //             this.guessedLetter = [e.key]
 //         }
 
-        
+
 // Hangman.prototype.getPuzzle = function(){
 //     let puzzle = '';
 //     this.word.forEach((letter)=>{
@@ -53,25 +53,51 @@
 // })
 
 // hardCoded code
-const Hangman = function(word,noOfGuessesLeft){
+const Hangman = function (word, noOfGuessesLeft) {
     this.word = word.toLowerCase().split('');
     this.noOfGuessesLeft = noOfGuessesLeft;
-    this.guessedLetter = ["c"] ; //hardCode value
+    this.guessedLetter = []; //hardCode value
 }
 
 
-Hangman.prototype.getPuzzle = function(){
-let puzzle = '';
-this.word.forEach((letter)=>{
-if(this.guessedLetter.includes(letter) || letter === ' '){
-    puzzle = puzzle + letter;
-} else {
-    puzzle = puzzle + '*'
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = '';
+    this.word.forEach((letter) => {
+        if (this.guessedLetter.includes(letter) || letter === ' ') {
+            puzzle = puzzle + letter;
+        } else {
+            puzzle = puzzle + '*'
+        }
+    })
+
+    return puzzle
 }
+
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase();
+    const isUnique = !this.guessedLetter.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+    if (isUnique) {
+        this.guessedLetter.push(guess);
+    }
+    if (isUnique && isBadGuess) {
+        this.noOfGuessesLeft = this.noOfGuessesLeft - 1;
+    }
+}
+window.addEventListener('keypress', function (e) {
+    // the below line gives the name and character code of the key pressed
+    // console.log(e.key, "->", e.charCode);
+    // since we need only the characters
+    const guess = String.fromCharCode(e.charCode)
+    console.log(guess);
 })
-
-return puzzle
-}
-const game1 = new Hangman("Cat")
-console.log(game1.word);
+const game1 = new Hangman("Cat", 2);
 console.log(game1.getPuzzle());
+console.log(game1.makeGuess("c"));
+
+// console.log(generateString(5));
+// program to generate random strings
+
+// declare all characters
+
+// 
